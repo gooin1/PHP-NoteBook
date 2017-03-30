f3ad3eecb7f7e45e84427c28748f0312fe0d450f
gooin <gooin@outlook.com>
2017-03-30 11:15:45 +0800

    优化文本框获取的文本

--- ConnectDatabase/php/run_query.php
+++ ConnectDatabase/php/run_query.php
@@ -6,10 +6,12 @@
  * Time: 20:05
  */
 require "database_connection.php";
+
+
 // 获取文本框输入的内容
 $query_text = $_REQUEST['query'];
-echo "query text is: " . $query_text . "<br>";
-
+// 转换为大写并删除空格用于检查关键字
+$upper_query_text =  trim(strtoupper($query_text));
 // 将查询结果保存到 result 中
 $result = mysqli_query($db, $query_text);
 
@@ -18,6 +20,30 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+
+$return_rows = false;
+// 保存关键字出现的位置
+$location = strpos($upper_query_text, "CRATE");
+// 如果没有关键字, 则继续向下查找
+if ($location === false || $location>0){
+    $location = strpos($upper_query_text, "UPDATE");
+    if ($location === false || $location>0) {
+        $location = strpos($upper_query_text, "DELETE");
+        if($location === false || $location>0){
+            $location = strpos($upper_query_text, "DROP");
+            if($location === false || $location>0){
+                $location = strpos($upper_query_text, "INSERT");
+                if ($location === false || $location>0) {
+                    // 代码运行到此处说明输入的查询语句没有上面几个关键词
+                    // 应该是返回结果行
+                    $return_rows = true;
+                }
+            }
+        }
+    }
+}
+
+if($return_rows){
     // 输出有几行
     printf("return %d rows", mysqli_num_rows($result));
     // 输出每行的名字
@@ -26,6 +52,8 @@ if (mysqli_connect_errno()) {
         echo "<li>{{$row[0]}}</li>";
     }
     echo "<ul>";
-
-
+} else {
+    echo "<p>你输入的 SQL 语句已经成功执行</p>";
+    echo "<p>{{$query_text}}</p>";
+}
 
