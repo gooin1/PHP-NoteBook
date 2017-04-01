89b029a3ffdb46c13a57fb2e692923c9984f0470
gooin <gooin@outlook.com>
2017-04-01 18:59:15 +0800

    comment added

--- ConnectDatabase/php/create_user.php
+++ ConnectDatabase/php/create_user.php
@@ -6,20 +6,24 @@
  * Time: 13:32
  */
 require "database_connection.php";
-
+// 获取表单中的姓名 删除无关字符
 $name = trim($_REQUEST['name']);
 $email = trim($_REQUEST['email']);
 $weibo = trim($_REQUEST['weibo']);
 $regex = "/^\s*@/";
+// 检测填入的微博名字中是否有 @ 符号
 if(!preg_match($regex, $weibo)){
     $weibo = "@" . $weibo;
 }
+// 获取不带 @ 的微博 id
 $weibo_id = str_replace("@", "", $weibo);
 $weibo_url = "http://weibo.com/" . $weibo_id;
-
+// SQL 语句
 $insert_sql = "INSERT INTO users(name, email, weibo, weibo_url)". "VALUES('{$name}','{$email}','{$weibo}','{$weibo_url}');";
+// 执行 SQL 语句
 mysqli_query($db, $insert_sql)or die(mysqli_error($db));
 ?>
+<!--在 html 中表现出来-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -27,7 +31,7 @@ mysqli_query($db, $insert_sql)or die(mysqli_error($db));
     <link href="../../css/bootstrap.css" type="text/css" rel="stylesheet">
 </head>
 <body class="container">
-<div class="alert alert-success">
+<div class="alert alert-success" style="margin-top: 10%">
     <h1>User Created Successful</h1>
 </div>
 <div class="alert alert-warning">
